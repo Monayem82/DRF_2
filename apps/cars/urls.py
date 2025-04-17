1436bba98978f9e0be3214065867b78bf11273a4
Monayem <mamonayem6@gmail.com>
2025-04-17 23:53:12 +0600

    adds

--- apps/cars/urls.py
+++ apps/cars/urls.py
@@ -19,6 +19,8 @@ urlpatterns = [
     path('review/api/',views.ReviewApiview.as_view(),name="reviews"),
     path('review/api/<int:pk>',views.ReviewDetailsView.as_view(),name="review"),
 
+    path('showroom/<int:pk>/review/',views.ReviewsListView.as_view(),name="review_list")
+
     path('showroom/api/',views.ShowroomApiView.as_view(),name="showroomApi"),
     path('showroom/api/<int:pk>',views.ShowroomApiDetailsView.as_view(),name="showroomApiDetails"),
     
