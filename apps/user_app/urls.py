f0aa86d651f5a21e32f442e1d7ec5af31d0c2a41
Monayem <mamonayem6@gmail.com>
2025-04-20 21:08:37 +0600

    logout

--- apps/user_app/urls.py
+++ apps/user_app/urls.py
@@ -5,5 +5,6 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns =[
     path('login/',obtain_auth_token,name="login"),
-    path('register/',views.Registration_view,name='register')
+    path('register/',views.Registration_view,name='register'),
+    path('logout/',views.DeleteUserToken,name='logout'),
 ]
--- apps/user_app/views.py
+++ apps/user_app/views.py
@@ -3,7 +3,17 @@ from apps.user_app.serializers import RegistrationSerializer
 
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,authentication_classes
+from rest_framework.authtoken.models import Token
+from rest_framework.authentication import TokenAuthentication
+
+@api_view(['POST',])
+@authentication_classes([TokenAuthentication])
+def DeleteUserToken(request):
+    if request.method=='POST':
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
+
 
 @api_view(['GET','POST'])
 def Registration_view(request):
